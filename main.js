55ab1499c9efad4966e7c9a475d90daa4bd308c0
sultonqulnd <sultonqulnd@gmail.com>
2024-04-26 09:45:09 +0500

    project completed

---
+++ main.js
@@ -0,0 +1,50 @@
+const emojis = [
+  "😍",
+  "😍",
+  "😜",
+  "😜",
+  "😂",
+  "😂",
+  "😎",
+  "😎",
+  "😡",
+  "😡",
+  "😭",
+  "😭",
+  "🥶",
+  "🥶",
+  "🤯",
+  "🤯",
+];
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.classList.add("item");
+
+  box.onclick = (e) => {
+    e.target.classList.add("boxOpen");
+    setTimeout(() => {
+      if (document.querySelectorAll(".boxOpen").length > 1) {
+        if (
+          document.querySelectorAll(".boxOpen")[0].innerHTML ==
+          document.querySelectorAll(".boxOpen")[1].innerHTML
+        ) {
+          document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+          document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
+
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+
+          if (document.querySelectorAll(".boxMatch").length == emojis.length) {
+            window.location.reload();
+          }
+        } else {
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+        }
+      }
+    }, 500);
+  };
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".container .game").appendChild(box);
+}
